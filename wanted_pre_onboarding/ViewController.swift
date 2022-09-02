30225a52d7605fec3de5caceb256ae6944fc7e7d
valseLee <sollleky72@gmail.com>
2022-09-02 11:44:29 +0900

    init: init

---
+++ wanted_pre_onboarding/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  wanted_pre_onboarding
+//
+//  Created by 이승준 on 2022/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		configUI()
+	}
+
+	func configUI() {
+		view.backgroundColor = .clear
+	}
+}
+
