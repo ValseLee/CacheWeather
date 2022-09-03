a3e6442d05f84c850fb606b48cf1cace8e46550e
valseLee <sollleky72@gmail.com>
2022-09-03 22:31:28 +0900

    add: 날씨 구조체 미리생성

---
+++ wanted_pre_onboarding/ViewModel/WeatherViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherIconView.swift
+//  wanted_pre_onboarding
+//
+//  Created by 이승준 on 2022/09/03.
+//
+
+import Foundation
+import UIKit
+
+struct WeatherIconViewModel {
+	var weatherIcon: UIImage?
+	
+	init() {
+		
+	}
+}
